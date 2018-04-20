fea95a33e1be03e07e2c87e699b4ecdbd3b30a65
Polaaa5 <32522839+Polaaa5@users.noreply.github.com>
2018-04-21 02:27:33 +0500

    изменения

---
+++ 3zadanie.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int i,j,x;
+const int n=2;
+const int m=2;
+int main(void) {
+	
+int matrix_A[n][m] = {
+	{0,1},
+	{0,0}
+};
+int matrix_B[n][m];
+
+(A + i*m+j)=x; 
+k = i*M + j;
+{for (long i = k; i < n; ++i)
+{
+  A[i] = A[i + 1];
+}
+--n;}
+	for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            matrix_A[i][j] = matrix_B[j][i];
+        }
+	}
+for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            printf("%3i", matrix_A[i][j]);
+        }
+}
+
+	return 0;
+}
